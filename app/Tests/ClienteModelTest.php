9c3af4512bd41dd85c45ffe0689211bfe18f14d5
Jose <13joselon@gmail.com>
2021-11-22 09:48:59 -0500

    se agregó el mockClienteModel

--- app/Tests/ClienteModelTest.php
+++ app/Tests/ClienteModelTest.php
@@ -1,4 +1,5 @@
 <?php
+require_once(__DIR__ . '/../Controllers/ClienteController.php');
 require_once(__DIR__ . '/../Models/ClienteModel.php');
 require_once(__DIR__ . '/../Db/db.php');
 
@@ -11,5 +12,27 @@ class ClienteModelTest extends TestCase
     {
         $test = new ClienteModel();
         $this->assertNotEmpty($test->listar());
+    }
+    public function puntosProveedor()
+    {
+        return [
+            'Caso 1' => [500, 'Bronce'],
+            'Caso 2' => [501, 'Silver'],
+            'Caso 3' => [1000, 'Silver'],
+            'Caso 4' => [1001, 'Gold']
+        ];
+    }
+    /**
+     * @dataProvider puntosProveedor
+     */
+    public function testCalcularCategoria($puntos, $categoria)
+    {
+        $mockClienteModel = $this->getMockBuilder('ClienteModel')
+                            ->onlyMethods(array('obtenerTotalPuntos'))
+                            ->getMock();
+        $mockClienteModel->expects($this->once())
+                         ->method('obtenerTotalPuntos')
+                         ->will($this->returnValue($puntos));
+        $this->assertEquals($categoria, $mockClienteModel->calcularCategoria(1));
     }
 }
